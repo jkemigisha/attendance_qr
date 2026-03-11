565bd5bd33606ceec3adaf5e56147b37906245fa
jkemigisha <kjoseph1232018@gmail.com>
2026-03-11 11:34:38 +0300

    commit

--- src/components/StudentDashboard.tsx
+++ src/components/StudentDashboard.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { supabase } from "@/integrations/supabase/client";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { LogOut, QrCode, CheckCircle2, Calendar, Bell } from "lucide-react";
+import { LogOut, QrCode, CheckCircle2, Calendar } from "lucide-react";
 import { toast } from "sonner";
 import { useNavigate } from "react-router-dom";
 import QRScanner from "./QRScanner";
